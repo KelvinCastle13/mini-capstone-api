dfd7133e3fb3179b00ea5efebb033fbd8c72ba69
KelvinCastle13 <kelvincastle13@gmail.com>
2025-07-07 21:34:24 -0400

    attempt #5

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173", "https://mini-capstone-api-v7ht.onrender.com", "mini-capston-frontend.netlify.app"
+    origins "localhost:5173", "https://mini-capstone-api-v7ht.onrender.com", "https://mini-capston-frontend.netlify.app"
     resource "*",
              headers: :any,
              methods: %i[get post put patch delete options head],
